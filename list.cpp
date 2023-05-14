5b8c50a4e3836b1d43a647c440da3df43b706369
txmedic436 <ccolpitts1@yahoo.com>
2023-05-13 19:10:02 -0500

    Updated the file changes

--- list.cpp
+++ list.cpp
@@ -4,41 +4,108 @@
 #include "list.h"
 
 List::List(){
-	if(!OpenFile(m_filepath, std::ios::in)){
+	if(OpenFile() == 1){
 		exit(EXIT_FAILURE);
 	}
+	else LoadFile();
 }
 
 List::List(std::string filepath){
 	m_filepath = filepath;
-	if(!OpenFile(m_filepath, std::ios::in)){
+	if(OpenFile() == 1){
 		exit(EXIT_FAILURE);
+	}
+	else LoadFile();
+}
+
+void List::Add(Task task){
+	m_list.push_back(task);
+}
+
+void List::PrintTask(Task* task){
+	std::cout << task->title << std::endl << "\t" << task->detail << std::endl;
+}
+
+void List::PrintList(){
+	for(int i = 0; i < m_list.size(); i++){
+		std::cout << i + 1 << ": ";
+		PrintTask(&m_list[i]);
 	}
 }
 
-bool File:: OpenFile(std::ios::fmtflags flag){
-	file.open(m_filepath, flag);
-	if(!file.is_open()){
-		std::cerr << "Unable to open " << m_filepath << std::endl;
-		if(flag == std::ios::in){
-			char option;
-			std::cout << "Would you like to create it(y/n)? ";
-			std::cin.get(option);
-			if(option == 'y' || option == 'Y'){
-				OpenFile(m_filepath, std::ios::out);
-				file.close();
-				file.open(m_filepath, std::ios::in);
-				return true;
-			}
-			else return false;
-		}
-	else return true;
+void List::RemoveTask(size_t taskID){
+	if(taskID > m_list.size()){
+		std::cerr << "Invalid task ID\n";
+		exit(EXIT_FAILURE);
+	}
+	m_list.erase(m_list.begin() + (taskID - 1));
 }
 
 
+//This function keeps deleting the element after the change is made
+void List::EditTask(size_t taskID){
+	std::string buffer;
+	
+	std::cout << "Current Title: " << m_list.at(taskID - 1).title << std::endl;
+	std::cout << "New Title: ";
+	getline(std::cin, buffer);
+	if(!buffer.empty()){
+		m_list.at(taskID - 1).title = buffer;
+	}
+	std::cout << "Current Description: " << m_list.at(taskID - 1).detail << std::endl;
+	std::cout << "New Descritpion: ";
+	getline(std::cin, buffer);
+	if(!buffer.empty()){
+		m_list.at(taskID - 1).detail = buffer;
+	}
+}
+
+int List::OpenFile(){
+	m_file.open(m_filepath, std::ios::in);
+	if(m_file.fail()){
+		std::cerr << "Unable to open " << m_filepath << std::endl;
+		char option;
+		std::cout << "Would you like to create it(y/n)? ";
+		std::cin.get(option);
+		if(option == 'y' || option == 'Y'){
+			m_file.open(m_filepath, std::ios::out);
+			m_file.close();
+			m_file.open(m_filepath, std::ios::in);
+			return 0;
+		}
+		else return 1;
+	}
+	else return 0;
+}
+
+void List::LoadFile(){
+	std::string line;
+	while(getline(m_file, line)){
+		Task buffer_task = {};
+		std::stringstream linestream(line);
+		std::string value;
+
+		getline(linestream, value, ',');
+		buffer_task.title = value;
+		getline(linestream, value);
+		buffer_task.detail = value;
+		m_list.push_back(buffer_task);
+	}
+
+	m_file.close();
+}
+
+void List::WriteFile(){
+	m_file.open(m_filepath, std::ios::trunc | std::ios::out);
+	for(int i = 0; i < m_list.size(); i++){
+		m_file << m_list[i].title << "," << m_list[i].detail << std::endl;
+	}
+	m_file.close();
+}
+
 List::~List(){
-	if(file.open()){
-		file.close();
+	if(m_file.is_open()){
+		m_file.close();
 	}
 }
 
--- list.h
+++ list.h
@@ -4,24 +4,35 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
+
+struct Task {
+	std::string title;
+	std::string detail;
+};
 
 class List {
 public:
-	void openFile(std::string);
-	int SetFilePath(std::string);
-	void AddItem();
-	void RemoveItem(int);
-	void EditItem(int);
+	List();
+	List(std::string);
+	~List();
+	void Add(Task);
+	void EditTask(size_t);
+	void RemoveTask(size_t);
+	void PrintTask(Task*);
+	void PrintList();
+	void WriteFile();
+public:
+	std::vector<Task> m_list;
 
 private:
-	std::fstream file;
-	std::string m_filepath = ".todo.dat"	//Default file path
-	struct Task {
-		std::string title;
-		std::string detail;
-	};
-}
+	int OpenFile();
+	void LoadFile();
+private:
+	std::fstream m_file;
+	std::string m_filepath;
+};
 
 
 #endif
